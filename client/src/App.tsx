cd46e4391472531da3f3e967871a7ef5e554aacf
foxt451 <svpetrenko123@gmail.com>
2022-06-27 13:28:37 +0300

    Final initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,7 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { Counter } from "./features/counter/Counter";
+import React from "react";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await axios.post("http://localhost:3000/api/v1", {
-        query: `
-        mutation Login($username: String!) {
-          login(username: $username) {
-            username
-          }
-        } 
-      `,
-        variables: {
-          username: "test",
-        },
-      });
-      console.log(data);
-    };
-    fetchData();
-  }, []);
   return <div className="App"></div>;
 }
 

2eda590bf88119f039a304f3ea52a88ec5ca865e
foxt451 <svpetrenko123@gmail.com>
2022-07-07 14:49:19 +0300

    Make decent Error Box

--- client/src/components/ErrorBox.tsx
+++ client/src/components/ErrorBox.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
+import { Alert } from "@mui/material";
 
 interface IProps {
   message: string;
 }
 const ErrorBox: FC<IProps> = ({ message }) => {
-  return <div>Error: {message}</div>;
+  return <Alert severity="error">{message}</Alert>;
 };
 
 export default ErrorBox;
